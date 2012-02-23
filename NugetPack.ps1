82302c899e98ce2806544209708fcf242dc0dedb
Chris Hogan <chogan@logikos.com>
2012-02-23 14:29:30 -0500

    Added Silverlight 5.0 project, updated nuget build

--- NugetPack.ps1
+++ NugetPack.ps1
@@ -1,12 +1,21 @@
-$srcRoot = '.\src'                     # relative to script directory
-$versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
+
+$srcRoot = '.\src'                       # relative to script directory
+$buildFile = 'Utility.sln'            # relative to $srcRoot
+$buildConfiguration = 'Release'
+$nuspecFile = 'Utility\Utility.nuspec'   # relative to $srcRoot
+$versionFile = 'SharedAssemblyInfo.cs'   # relative to $srcRoot
 $outputPath = "$home\Dropbox\Packages"
 
-Import-Module NugetUtilities
+Import-Module BuildUtilities
 
+$versionFile =Resolve-Path(Join-Path $srcRoot $versionFile)
+$buildFile = Resolve-Path(Join-Path $srcRoot $buildFile)
+$nuspecFile = Resolve-Path(Join-Path $srcRoot $nuspecFile)
+
+$version = Get-Version $versionFile
+  
 New-Path $outputPath
+#Invoke-Build $buildFile $buildConfiguration
+New-Package $nuspecFile $version $outputPath
 
-$version = Get-Version (Join-Path $srcRoot $versionFile)
-
-Pack-Project Utility $srcRoot $version $outputPath
-
+Remove-Module BuildUtilities
