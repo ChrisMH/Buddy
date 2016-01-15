c5fae67918a103726a4c64f0b8081db26a746fbe
Chris Hogan <hogancm@gmail.com>
2016-01-14 22:29:38 -0500

    Fixed nuspec

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible( false )]
 
-[assembly: AssemblyVersion( "0.7.1.4" )]
-[assembly: AssemblyFileVersion("0.7.1.4")]
-[assembly: AssemblyInformationalVersion("0.7.1.4")]
+[assembly: AssemblyVersion( "0.7.1.5" )]
+[assembly: AssemblyFileVersion("0.7.1.5")]
+[assembly: AssemblyInformationalVersion("0.7.1.5")]
