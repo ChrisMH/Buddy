e1737b39e5720808d045eb4a6c5236c7be9e2699
Chris Hogan <hogancm@gmail.com>
2016-01-20 08:29:48 -0500

    Added multiple .NET version builds

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible( false )]
 
-[assembly: AssemblyVersion( "0.7.1.5" )]
-[assembly: AssemblyFileVersion("0.7.1.5")]
-[assembly: AssemblyInformationalVersion("0.7.1.5")]
+[assembly: AssemblyVersion( "0.7.2.0" )]
+[assembly: AssemblyFileVersion("0.7.2.0")]
+[assembly: AssemblyInformationalVersion("0.7.2.0")]
