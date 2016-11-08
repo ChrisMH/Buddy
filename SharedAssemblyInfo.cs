79fbb12094dddcae33be8006825de47c3e6acd8c
Chris Hogan <hogancm@gmail.com>
2016-11-08 14:24:22 -0500

    Added GetEnumValueFromDescription, added unit tests for EnumExtensions

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible( false )]
 
 
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.3")]
+[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: AssemblyInformationalVersion("1.6.3")]
