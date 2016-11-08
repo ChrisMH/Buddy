da8dc86ce8761af1d5dde2798ae93eac35050932
Chris Hogan <hogancm@gmail.com>
2016-11-08 14:24:22 -0500

    Added GetEnumValueFromDescription, added unit tests for EnumExtensions

---
+++ Buddy45.Test/Enum/EnumExtensionsTest.cs
@@ -0,0 +1,51 @@
+﻿using Buddy.Enum;
+using NUnit.Framework;
+
+namespace Buddy.Test.Enum
+{
+    public enum TestEnumType
+    {
+        [System.ComponentModel.Description("zero")]
+        Zero,
+        
+        [System.ComponentModel.Description("one")]
+        One,
+
+        Two,
+        
+        [System.ComponentModel.Description("three")]
+        Three
+    }
+
+    public class EnumExtensionsTest
+    {
+        [TestCase(TestEnumType.Zero, "zero")]
+        [TestCase(TestEnumType.One, "one")]
+        [TestCase(TestEnumType.Three, "three")]
+        public void GetDescription_ReturnsDescription(TestEnumType value, string expected)
+        {
+            Assert.AreEqual(expected, value.GetDescription());
+        }
+
+        [TestCase(TestEnumType.Two)]
+        public void GetDescription_ReturnsEmptyIfNoDescription(TestEnumType value)
+        {
+            Assert.AreEqual(string.Empty, value.GetDescription());
+        }
+
+        [TestCase("zero", TestEnumType.Zero, TestEnumType.One)]
+        [TestCase("one", TestEnumType.One, TestEnumType.Zero)]
+        [TestCase("three", TestEnumType.Three, TestEnumType.Zero)]
+        public void GetEnumValueFromDescription_ReturnsValue(string description, TestEnumType expected, TestEnumType defaultValue)
+        {
+            Assert.AreEqual(expected, description.GetEnumValueFromDescription(defaultValue));
+        }
+
+        [TestCase("two")]
+        public void GetEnumValueFromDescription_ReturnsDefaultValueIfDescriptionNotFound(string description)
+        {
+            Assert.AreEqual(TestEnumType.Zero, description.GetEnumValueFromDescription(TestEnumType.Zero));
+        }
+
+    }
+}
--- Buddy45/Enum/EnumExtensions.cs
+++ Buddy45/Enum/EnumExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 
 namespace Buddy.Enum
 {
@@ -7,13 +9,31 @@ namespace Buddy.Enum
     {
         public static string GetDescription(this System.Enum source)
         {
-            var fieldInfo = source.GetType().GetField(source.ToString());
+            return GetDescription(source.GetType().GetField(source.ToString()));         
+        }
 
+        public static TEnum GetEnumValueFromDescription<TEnum>(this string description, TEnum defaultValue)
+        {
+            if(!typeof(TEnum).IsEnum)
+                throw new ArgumentException("TEnum must be an enumeratedType", nameof(TEnum));
+
+            foreach(var value in System.Enum.GetValues(typeof(TEnum)))
+            {
+                var field = typeof(TEnum).GetField(value.ToString());
+                if(GetDescription(field).Equals(description))
+                    return (TEnum)value;
+            }
+            return defaultValue;
+        }
+
+        public static string GetDescription(FieldInfo fieldInfo)
+        {
             var descriptionAttrs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>().ToArray();
             if (descriptionAttrs.Any())
                 return descriptionAttrs[0].Description;
 
             return string.Empty;
         }
+
     }
 }
