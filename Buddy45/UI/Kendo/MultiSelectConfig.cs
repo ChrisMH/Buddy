769301feee853048808ececde18a24b865a8f40f
Chris Hogan <hogancm@gmail.com>
2017-02-20 20:10:55 -0500

    Added MultiSelectConfig

---
+++ Buddy45/UI/Kendo/MultiSelectConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Buddy.UI.Kendo
+{
+    public class MultiSelectValue<TId>
+    {
+        public TId Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class MultiSelectConfig<TId, TValue>
+        where TValue : MultiSelectValue<TId>
+    {
+        public List<TId> Defaults { get; set; }
+        public List<TValue> Values { get; set;}
+    }
+}
